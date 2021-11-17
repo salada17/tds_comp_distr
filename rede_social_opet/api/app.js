a59c9b8a0785005f408d0876fb1201d4d62b753c
Matheus Rosa <matheus.alves.rosa@outlook.com>
2021-11-16 21:39:15 -0300

    Update app.js
    cors and jwt middleware

--- rede_social_opet/api/app.js
+++ rede_social_opet/api/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
-import { routes } from './routes/v1/index.js';
+import routes from './routes/v1/index.js';
+import cors from 'cors'
+import authMiddleware from './middlewares/auth.js';
+import * as dotEnvSafe from 'dotenv-safe';
+//import * as oauthServer from 'express-oauth-server';
+//import model from './oauth2Model.js';
+
+dotEnvSafe.config();
 
 const app = express();
+const port = 3001;
 const router = express.Router();
-const port = 3000;
+router.use(authMiddleware);
 
+//app.oauth = oauthServer({ model });
+
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routes(router));
+
+//app.post('/oauth/token', app.oauth.token());
 
 app.listen(port, () => {
   console.log(`Application running on port ${port}`);
